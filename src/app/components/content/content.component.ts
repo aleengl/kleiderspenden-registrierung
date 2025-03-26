a8d518e9f480e1adacda04f08df57b5dc3d4d82b
Alexander Engl <alexanderengl73@gmail.com>
2025-03-26 19:18:15 +0100

    Add content to the content component

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RegisterFormComponent} from '../register-form/register-form.component';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RegisterFormComponent
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
